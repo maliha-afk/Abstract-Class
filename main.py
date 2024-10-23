b7991435d4389cba71fe5386fff96cf1a6c67946
maliha-afk <24amaliha@gmail.com>
2024-10-23 21:55:01 +0600

    main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+class honda:
+    def speed(self):
+        print("honda's max speed: 200km/h")
+
+class audi:
+    def speed(self):
+        print("audi's max speed: 250km/h")
+
+class BMW:
+    def speed(self):
+        print("BMW's max speed: 300km/h")
+
+class mercedes:
+    def speed(self):
+        print("mercedes's max speed: 200km/h")
+
